a10b9422f21e5dfed1f9cc4d02dd30a0e9e4f257
Jason Turley <jasonturley0@gmail.com>
2017-11-10 22:59:33 -0600

    Add insert at head and delete at position functions

--- Linked Lists/singly-linked-list.c
+++ Linked Lists/singly-linked-list.c
@@ -2,9 +2,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node {
+struct Node 
+{
     int data;
     struct Node *next;
+};
+
+// Given the start of a list and an int, inserts a new node at the front of a list
+void push(struct Node **head, int new_data)
+{
+    // If there's no list, exit function
+    if (!head)
+    {
+        puts("Error: no list found");
+        return;
+    }
+
+    struct Node *new_node = (struct Node*) malloc(sizeof(struct Node));
+    
+    new_node->data = new_data;
+    new_node->next = *head;
+    *head = new_node;
+}
+
+// Deletes node at k, where k is the position of node
+void deleteNodeK(struct Node **head, int k)
+{
+    if (!head)
+    {
+        puts("Error: no list found");
+        return;
+    }
+
+    struct Node *temp, *prev;
+    temp = *head;
+    prev = *head;
+
+    // Select node directly before the one to be delete
+    // change what it points to
+    int i;
+    for (i = 1; i < k-1; i++)
+    {
+        prev = prev->next;
+    }
+    temp = prev->next;
+    prev->next = temp->next;
+
+    // Delete selected node
+    free(temp);
 }
 
 // This function prints the contents of a linked list starting from a given node
@@ -15,30 +60,22 @@ void printList(struct Node *n)
         printf(" %d", n->data);
         n = n->next;
     }
+    puts("\n");
 }
-
-
 
 int main(int argc, char *argv[])
 {
+    // start with empty list
     struct Node* head = NULL;
-    struct Node* second = NULL;
-    struct Node* third = NULL;
-     
-    // allocate 3 nodes in the heap  
-    head  = (struct Node*)malloc(sizeof(struct Node)); 
-    second = (struct Node*)malloc(sizeof(struct Node));
-    third  = (struct Node*)malloc(sizeof(struct Node));
-    
-    head->data = 1; //assign data in first node
-    head->next = second; // Link first node with second   
-    
-    second->data = 2; //assign data to second node
-    second->next = third;  
-    
-    third->data = 3; //assign data to third node
-    third->next = NULL;
-     
+
+    push(&head, 1);
+    push(&head, 2);
+    push(&head, 3);
+    push(&head, 4);
+    puts("Current list:\t");
+    printList(head);
+    puts("List after deletion:\t");
+    deleteNodeK(&head, 3);
     printList(head);
     
     return 0;
