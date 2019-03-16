7ab8c3ade8cd63489aa0c3873e8ee9eb742d3e04
Jason Turley <jturle2@illinois.edu>
2019-03-16 00:11:34 -0500

    add test cases

--- C/Vector/main.c
+++ C/Vector/main.c
@@ -4,6 +4,16 @@
 
 #include "vector.h"
 
+void run_all_tests();
+
+int main()
+{
+	run_all_tests();
+
+    	return 0;
+}
+
+
 void test_push()
 {
 	size_t i;
@@ -13,15 +23,64 @@ void test_push()
 	assert(size(v) == 0);
 	assert(capacity(v) == MIN_CAPACITY);
 
-	for (i = 0; i < n; i++) {
+	for (i = 0; i < n; i++)
 		push(v, (int) i+1);
-	}
 
 	assert(size(v) == n);
+
+	vector_destroy(v);	// no memory leaks
 }
 
-int main()
+void test_at()
+{
+	struct vector *v = vector_create(32);
+	int ret;
+	size_t i;
+
+	for (i = 0; i < 16; i++) {
+		push(v, (int) i);
+		ret = at(v, i);
+		assert(ret == (int) i);
+	}
+	
+	vector_destroy(v);
+}
+
+void test_resize()
+{
+	struct vector *v = vector_create(16);
+	size_t i;
+
+	for (i = 0; i < 20; i++) 
+		push(v, (int) i);
+
+	assert(capacity(v) == 32);
+	assert(size(v) == 20);
+
+	vector_destroy(v);
+}
+
+void test_insert()
+{
+	struct vector *v = vector_create(10);
+	size_t i;
+
+	for (i = 0; i < 10; i++)
+		push(v, (int) i);
+
+	print_vector(v);
+	insert(v, 4, 88);
+	print_vector(v);
+
+	assert(size(v) == 11);
+	assert(at(v, 4) == 88);
+	vector_destroy(v);
+}
+
+void run_all_tests()
 {
 	test_push();
-    	return 0;
+	test_at();
+	test_resize();
+	test_insert();
 }
