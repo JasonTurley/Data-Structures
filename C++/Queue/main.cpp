4fdf7a4cc001729834c9885d2b31fbf57ff5d748
Jason Turley <jasonturley0@gmail.com>
2019-06-21 12:57:13 -0500

    add test cases

--- C++/Queue/main.cpp
+++ C++/Queue/main.cpp
@@ -1,18 +1,54 @@
+#include <cassert>
 #include <iostream>
 
 #include "queue.h"
 
 using namespace std;
 
+void testCopyConstrcutor()
+{
+        Queue<int> q1(4);
+
+        q1.enqueue(100);
+        q1.enqueue(200);
+
+        Queue<int> q2 = q1;
+
+        assert(q1.size() == q2.size());
+        assert(q1.capacity() == q2.capacity());
+        assert(q1.front() == q2.front());
+        assert(q1.back() == q2.back());
+}
+
+void testEnqueue()
+{
+        const int N = 10;
+        Queue<int> q(N);
+
+        assert(q.empty() == true);
+
+        // fill queue halfway
+        for (int i = 1; i <= N/2; i++)
+                q.enqueue(i);
+
+        assert(q.capacity() == N);
+        assert(q.size() == N/2);
+        assert(q.front() == 1);
+        assert(q.back() == N/2);
+        assert(q.full() == false);
+
+}
+
+void testAll()
+{
+        testEnqueue();
+        //testDequeue();
+        testCopyConstrcutor();
+}
+
 int main()
 {
-        Queue<int> q(10);
-
-        cout << "size: " << q.size() << endl;
-        cout << "capacity: " << q.capacity() << endl;
-
-        cout << "front: " << q.front() << endl;
-        cout << "back: " << q.back() << endl;
+        testAll();
 
         return 0;
 }
