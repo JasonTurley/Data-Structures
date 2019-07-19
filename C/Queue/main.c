aecbaa24bb948eb420d8eda4ff79b63140bf0646
Jason Turley <jasonturley0@gmail.com>
2019-07-19 09:51:40 -0500

    add test case

--- C/Queue/main.c
+++ C/Queue/main.c
@@ -1,14 +1,51 @@
+#include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 #include "queue.h"
 
-int main()
+#define N (16)
+
+int *create_test_int_array(size_t size)
+{
+	int *arr = malloc(sizeof(int) * size);
+
+	if (!arr) {
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	for (size_t i = 0; i < size; i++)
+		arr[i] = i;
+
+	return arr;
+}
+
+void test_enqueue()
 {
 	queue_t *q = create_queue();
-	short x = 12;
-	enqueue(q, &x);
+	int *a = create_test_int_array(N);
+
+	for (int i = 0; i < N; i++)
+		enqueue(q, &(a[i]));
+
+	for (int i = 0; i < N; i++) {
+		int ret = *((int *)dequeue(q));
+		printf("%d ", ret);
+	}
+
+	printf("\n");
+	free(a);
 	destroy_queue(q);
+}
+void test_all()
+{
+	test_enqueue();
+}
+
+int main()
+{
+	test_all();
 
 	return 0;
 }
